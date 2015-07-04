6159183508e3b44b030f58a8e599a21d52f90214
Ian McCunn <ianmccunn@gmail.com>
2015-07-03 21:58:18 -0700

    directive controller

--- app/202-basic/script.js
+++ app/202-basic/script.js
@@ -4,20 +4,33 @@
 angular.module('app', []);
 
 angular.module('app').controller('mainCtrl', function($scope) {
- $scope.user = {
-   name: 'Ian McCunn',
-   something: 5,
-   address: {
-     street: 'PO Box 123',
-     city: 'Milk',
-     planet: 'Jupiter'
-   },
-   friends: [
-     'han',
-     'Leia'
-   ]
- }; 
-
+   $scope.user1 = {
+     name: 'Ian McCunn',
+     something: 5,
+     address: {
+       street: 'PO Box 123',
+       city: 'Milk',
+       planet: 'Jupiter'
+     },
+     friends: [
+       'han',
+       'Leia'
+     ]
+   }; 
+  
+   $scope.user2 = {
+     name: 'bob',
+     something: 33,
+     address: {
+       street: 'PO Box 333',
+       city: 'LA',
+       planet: 'Mars'
+     },
+     friends: [
+       'Mom',
+       'Milk'
+     ]
+   }; 
 
 });
 
@@ -26,6 +39,9 @@ angular.module('app').directive('userInfoCard', function() {
   return {
     restrict: 'A',
     templateUrl: 'userInfoCard.html',
+    scope: {
+      user: '=' 
+    },
     controller: function($scope) {
        
       $scope.knightMe = function(user) {
